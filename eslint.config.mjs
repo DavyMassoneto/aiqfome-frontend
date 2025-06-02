31bfe4464dca74062b34db94a5b0d8ca36548379
DavyMassoneto <davy.massoneto@yahoo.com>
2025-06-02 05:17:30 -0300

    🔧 config(next): Update Next.js and ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,36 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintPluginImport from 'eslint-plugin-import'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
+
+const importRules = {
+  plugins: {
+    import: eslintPluginImport,
+  },
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external'], ['internal'], ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+  },
+}
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  eslintPluginPrettierRecommended,
+  importRules,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+]
 
-export default eslintConfig;
+export default eslintConfig
